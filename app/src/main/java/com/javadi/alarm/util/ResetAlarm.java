51b6433d75f6fe92355da1ba1341293300974a96
Javadi <javadimehr01@gmail.com>
2019-10-31 21:23:31 +0330

    fix  prevent alarm active after boot while not alarm set

--- app/src/main/java/com/javadi/alarm/util/ResetAlarm.java
+++ app/src/main/java/com/javadi/alarm/util/ResetAlarm.java
@@ -20,39 +20,39 @@ public class ResetAlarm extends BroadcastReceiver {
             Cursor cursor=App.dbHelper.getAlarms();
                 if(cursor.moveToFirst()){
                     do{
-                        int pend=cursor.getInt(0);
-                        int H=cursor.getInt(cursor.getColumnIndex(DBC.hour));
-                        int M=cursor.getInt(cursor.getColumnIndex(DBC.minute));
-                        Calendar calendar=Calendar.getInstance();
-                        calendar.set(Calendar.HOUR_OF_DAY,H);
-                        calendar.set(Calendar.MINUTE,M);
-                        calendar.set(Calendar.SECOND,0);
-                        if(calendar.getTimeInMillis()< System.currentTimeMillis()){
-                            calendar.add(Calendar.DATE,1);
-                        }
-                        Intent intent2=new Intent(context, MyReceiver.class);
-                        intent2.setAction("com.javadi.alarm");
-                        AlarmManager alarmManager=(AlarmManager)context.getSystemService(context.ALARM_SERVICE);
-                        PendingIntent pendingIntent=PendingIntent.getBroadcast(context.getApplicationContext(),pend,intent2,PendingIntent.FLAG_UPDATE_CURRENT);
+                        if(cursor.getInt(3)==1){
+                            int pend=cursor.getInt(0);
+                            int H=cursor.getInt(cursor.getColumnIndex(DBC.hour));
+                            int M=cursor.getInt(cursor.getColumnIndex(DBC.minute));
+                            Calendar calendar=Calendar.getInstance();
+                            calendar.set(Calendar.HOUR_OF_DAY,H);
+                            calendar.set(Calendar.MINUTE,M);
+                            calendar.set(Calendar.SECOND,0);
+                            if(calendar.getTimeInMillis()< System.currentTimeMillis()){
+                                calendar.add(Calendar.DATE,1);
+                            }
+                            Intent intent2=new Intent(context, MyReceiver.class);
+                            intent2.setAction("com.javadi.alarm");
+                            AlarmManager alarmManager=(AlarmManager)context.getSystemService(context.ALARM_SERVICE);
+                            PendingIntent pendingIntent=PendingIntent.getBroadcast(context.getApplicationContext(),pend,intent2,PendingIntent.FLAG_UPDATE_CURRENT);
 
-                        if(Build.VERSION.SDK_INT>23){
-                            alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis(),pendingIntent);
-                        }
-                        else{
-                            alarmManager.set(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis(),pendingIntent);
-                        }
+                            if(Build.VERSION.SDK_INT>23){
+                                alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis(),pendingIntent);
+                            }
+                            else{
+                                alarmManager.set(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis(),pendingIntent);
+                            }
 
-                        if(Build.VERSION.SDK_INT<22){
-                            Intent alarmChanged = new Intent("android.intent.action.ALARM_CHANGED");
-                            alarmChanged.putExtra("alarmSet", true);
-                            context.sendBroadcast(alarmChanged);
-                            //alarmManager.set(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis(),pendingIntent);
+                            if(Build.VERSION.SDK_INT<22){
+                                Intent alarmChanged = new Intent("android.intent.action.ALARM_CHANGED");
+                                alarmChanged.putExtra("alarmSet", true);
+                                context.sendBroadcast(alarmChanged);
+                                //alarmManager.set(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis(),pendingIntent);
+                            }
+                            else{
+                                alarmManager.setAlarmClock(new AlarmManager.AlarmClockInfo(calendar.getTimeInMillis(),pendingIntent),pendingIntent);
+                            }
                         }
-                        else{
-                            alarmManager.setAlarmClock(new AlarmManager.AlarmClockInfo(calendar.getTimeInMillis(),pendingIntent),pendingIntent);
-                        }
-
-
                     }while (cursor.moveToNext());
                 }
         }
